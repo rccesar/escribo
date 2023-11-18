182dd241792b4404be84526df0f5072d70eea4eb
Renato Cesar <renato11cesar@hotmail.com>
2023-11-18 09:22:01 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [numero, setNumero] = useState('');
+  const [resultado, setResultado] = useState(null);
+  const [divisiveis, setDivisiveis] = useState([]);
+
+  const calcularSomatorio = () => {
+    const num = parseInt(numero, 10);
+    if (!isNaN(num)) {
+      const divisiveisArray = [...Array(num).keys()]
+        .filter(i => i % 3 === 0 || i % 5 === 0);
+      setDivisiveis(divisiveisArray);
+      
+      const soma = divisiveisArray.reduce((a, b) => a + b, 0);
+      setResultado(soma);
+    } else {
+      setResultado(null);
+      setDivisiveis([]);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="wrapper">
+        <h1 className='text'>Escribo Inovação para o Aprendizado</h1>
+        <label >
+          Digite um número:
+          <input style={{margin:10}} type="number" value={numero} onChange={(e) => setNumero(e.target.value)} />
+        </label>
+        <button className="Submit" onClick={calcularSomatorio}>Calcular Somatório</button>
+        {resultado !== null && (
+          <p >O somatório dos valores divisíveis por 3 ou 5 até {numero} é: {resultado}</p>
+        )}
+        {divisiveis.length > 0 && (
+          <p>Números divisíveis por 3 ou 5 até {numero}: {divisiveis.join(', ')}</p>
+        )}
       </header>
     </div>
   );
